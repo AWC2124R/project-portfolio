527edcbb138e9cf79bdeea646ba72e08fab844b9
AWC2124R <45724779+AWC2124R@users.noreply.github.com>
2022-10-19 20:17:19 +0900

    Update ModelBaselineClassifier.py

--- Fire Image Classifier/ModelBaselineClassifier.py
+++ Fire Image Classifier/ModelBaselineClassifier.py
@@ -6,14 +6,12 @@ from keras import layers, models
 from keras.applications import VGG16
 from keras.applications import VGG19
 
-# 이미지 크기 조정
 image_w = 640/10
 image_h = 360/10
 
 X = []
 Y = []
 
-# 이미지 받아오기, 각 데이터에서 N만큼 받아옴, N < 5000 사용 추천
 N = 4000
 for idex, categorie in enumerate(['fire', 'normal']):
     label = idex
@@ -46,13 +44,11 @@ tmp = list(zip(test_images, test_labels))
 np.random.shuffle(tmp)
 test_images, test_labels = zip(*tmp)
 
-# cv2 -> ndarray 변환
 train_images = np.array(train_images, dtype=np.float64)
 test_images = np.array(test_images, dtype=np.float64)
 train_labels = np.array(train_labels, dtype=np.float64)
 test_labels = np.array(test_labels, dtype=np.float64)
     
-# 이미지 값 정규화
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
@@ -60,7 +56,6 @@ test_images = test_images / 255.0
 pre_trained_vgg = VGG19(weights='imagenet', include_top=False, input_shape=(360//10, 640//10, 3))
 pre_trained_vgg.trainable = False
 
-# imagenet VGG16 + 후반 Dense 추가
 model = models.Sequential()
 model.add(pre_trained_vgg)
 model.add(layers.Flatten())
@@ -69,12 +64,10 @@ model.add(layers.Dropout(0.4))
 model.add(layers.Dense(32, activation='relu'))
 model.add(layers.Dense(2, activation='softmax'))
 
-# 모델 컴파일
 model.compile(optimizer='adam',\
               loss='sparse_categorical_crossentropy',\
               metrics=['accuracy'])
 
-# 모델 훈련
 history = model.fit(train_images, train_labels, epochs=5)
 
 test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=1)
